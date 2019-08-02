6c8a8ba26f440c4dffb4fe93d06dc2094d4c2b45
patriziotomato <patrick.thomas@staudacher.de>
2019-08-02 10:07:28 +0200

    Throw exceptions instead of handling logging within the package

--- src/Services/Project.php
+++ src/Services/Project.php
@@ -2,8 +2,6 @@
 
 namespace LaravelJira\Services;
 
-use Illuminate\Support\Facades\Log;
-use JiraRestApi\JiraException;
 use JiraRestApi\Project\ProjectService;
 use LaravelJira\Responses\Versions;
 
@@ -11,16 +9,13 @@ trait Project
 {
     /**
      * @param $projectName
+     *
      * @return Versions
      */
     public function projectVersions($projectName)
     {
         $proj = new ProjectService();
 
-        try {
-            return new Versions($proj->getVersions($projectName));
-        } catch (JiraException $e) {
-            Log::error("Could not get Versions of Project {$projectName}: " . $e->getMessage());
-        }
+        return new Versions($proj->getVersions($projectName));
     }
 }

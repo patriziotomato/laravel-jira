d4de2630705105fc056dac1b3b3ca134b3aa18a6
Patrick Thomas <github@patrick-thomas.de>
2023-01-03 08:11:08 +0100

    Laravel 9 Migration

--- src/JiraFacade.php
+++ src/JiraFacade.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Facade;
 
 class JiraFacade extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
-        return 'jira';
+        return Jira::class;
     }
 }

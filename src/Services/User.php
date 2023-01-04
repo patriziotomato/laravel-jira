f3f14c6eef47b7897b62b8e19c28f158cd61b9e2
Patrick Thomas <github@patrick-thomas.de>
2023-01-04 09:39:40 +0100

    Fix parsing scoopOS Version

--- src/Services/User.php
+++ src/Services/User.php
@@ -2,23 +2,24 @@
 
 namespace LaravelJira\Services;
 
+use JiraCloud\JiraException;
 use JiraCloud\User\UserService;
+use JsonMapper_Exception;
 use LaravelJira\Responses\Users;
 
 trait User
 {
     /**
-     * @return Users
+     * @throws JsonMapper_Exception
+     * @throws JiraException
      */
     public function users(): Users
     {
         $userService = new UserService();
 
-        return new Users(users: $userService->findUsers([
-            'accountId' => '.', // get all users.
+        return new Users(users: $userService->getUsers([
             'startAt' => 0,
             'maxResults' => 1000,
-            'includeInactive' => false,
             //'property' => '*',
         ]));
     }
